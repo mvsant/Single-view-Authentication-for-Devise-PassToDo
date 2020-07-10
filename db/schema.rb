df4edddcb9bacbaaf03c15270558ce41eaa6c0a5
marlon <marlon_veiga@hotmail.com>
2020-07-10 16:27:29 -0300

    Create todo scaffold.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_190518) do
+ActiveRecord::Schema.define(version: 2020_07_10_192103) do
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
