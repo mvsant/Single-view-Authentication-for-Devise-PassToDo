88a2381775c1ad1e01d032185875d64cb7656003
marlon <marlon_veiga@hotmail.com>
2020-07-10 20:55:49 -0300

    Override redirection to Devise custom pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :todos
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'user/sessions',
+    registrations: 'user/registrations'
+  }
+
   root "home#index"
   get 'pass' => "todos#pass"
 end
