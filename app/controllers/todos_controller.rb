fc44f9c47d44f272cecbdbc25ba9827f27ecfa40
marlon <marlon_veiga@hotmail.com>
2020-07-11 14:20:10 -0300

    Comment unused code.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -76,23 +76,25 @@ class TodosController < ApplicationController
       params.require(:todo).permit(:title, :description, :status)
     end
 
-    def resource_name
-      :user
-    end
-    helper_method :resource_name
-   
-    def resource
-      @resource ||= User.new
-    end
-    helper_method :resource
-   
-    def devise_mapping
-      @devise_mapping ||= Devise.mappings[:user]
-    end
-    helper_method :devise_mapping
-   
-    def resource_class
-      User
-    end
-    helper_method :resource_class
+#    ALTERNATIVE TO APPLICATION_HELPER ACTIONS
+
+#    def resource_name
+#      :user
+#    end
+#    helper_method :resource_name
+#   
+#    def resource
+#      @resource ||= User.new
+#    end
+#    helper_method :resource
+#   
+#    def devise_mapping
+#      @devise_mapping ||= Devise.mappings[:user]
+#    end
+#    helper_method :devise_mapping
+#   
+#    def resource_class
+#      User
+#    end
+#    helper_method :resource_class
 end
