1e9add41cfe1a135de076ce49c78446dcf4dc5c6
marlon <marlon_veiga@hotmail.com>
2020-07-11 14:20:51 -0300

    Add devise authenticated root rpute.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     registrations: 'user/registrations'
   }
 
-  root "home#index"
-  get 'pass' => "todos#pass"
+  root "home#index"                                  # Index
+  get 'pass' => "todos#pass"                         # Custom route for new auth
+  get '/user' => "todos#index", :as => :user_root    # Root after authenticate
 end
